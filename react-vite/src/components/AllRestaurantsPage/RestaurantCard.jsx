a5878947b5aef7815cc1ab88e3105f8d2a7741a6
Elliot <Heecher626@gmail.com>
2024-01-03 13:38:23 -0500

    styles

--- react-vite/src/components/AllRestaurantsPage/RestaurantCard.jsx
+++ react-vite/src/components/AllRestaurantsPage/RestaurantCard.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom"
+import { FaStar } from "react-icons/fa";
+
 export default function RestaurantCard({restaurant}) {
+    const navigate = useNavigate()
 
     return(
-        <div>
-            <div>{restaurant.name}</div>
-            <img src={restaurant.preview_image_url}/>
+        <div onClick={e => navigate(`/restaurants/${restaurant.id}`)} className="restaurant-card">
+            <div className="restaurant-card-name">{restaurant.name}</div>
+            <img className="restaurant-card-image" src={restaurant.preview_image_url}/>
+            <div className="restaurant-card-star-line">{restaurant.stars >= 0 ? (<><FaStar className="star" /> {restaurant.stars}</>) : 'No reviews'}</div>
         </div>
     )
 }
